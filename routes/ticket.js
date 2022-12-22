d40ae0a7fb81f17afedbfbf75ee334609fa3de73
alfazaatariq <m.alfazaatariq@gmail.com>
2022-12-22 18:30:23 +0700

    added backend

---
+++ routes/ticket.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const TicketController = require('../controllers/TicketController');
+const upload = require('../middleware/upload');
+const authenticate = require('../middleware/authenticate');
+
+// AUTHORIZATION
+router.get('/', authenticate, TicketController.index);
+// NON-AUTHORIZATION
+router.get('/getall', TicketController.index);
+
+router.post('/show', TicketController.show);
+router.post('/store', TicketController.store);
+router.post('/update', TicketController.update);
+router.post('/delete', TicketController.destroy);
+router.post('/searchtickets', TicketController.searchTickets);
+router.post('/searchtujuan', TicketController.searchTujuan);
+router.get('/searchasal', TicketController.searchAsal);
+
+module.exports = router;
